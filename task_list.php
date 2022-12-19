bc69d4284ce34b49dc387a33ff171ff612ff2df8
vaishnaviThakare <vaishnavithakre03@gmail.com>
2022-12-19 20:18:09 +0530

    .

--- task_list.php
+++ task_list.php
@@ -1,18 +1,10 @@
 <?php include'db_connect.php' ?>
 <div class="col-lg-12">
 	<div class="card card-outline card-success">
-		<div class="card-header">
-			<div class="card-tools">
-				<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="./index.php?page=new_project"><i class="fa fa-plus"></i> Add New project</a>
-			</div>
-		</div>
 		<div class="card-body">
 			<table class="table tabe-hover table-condensed" id="list">
 				<colgroup>
 					<col width="5%">
-					<col width="15%">
-					<col width="20%">
-					<col width="15%">
 					<col width="15%">
 					<col width="10%">
 					<col width="10%">
@@ -21,47 +13,74 @@
 				<thead>
 					<tr>
 						<th class="text-center">#</th>
-						<th>Project</th>
-						<th>Task</th>
-						<th>Project Started</th>
-						<th>Project Due Date</th>
-						<th>Project Status</th>
-						<th>Task Status</th>
+						<th>Task Name</th>
+						<th>Start Date</th>
+						<th>End Date</th>
 						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php
 					$i = 1;
-					$where = "";
-					if($_SESSION['login_type'] == 2){
-						$where = " where p.manager_id = '{$_SESSION['login_id']}' ";
-					}elseif($_SESSION['login_type'] == 3){
-						$where = " where concat('[',REPLACE(p.user_ids,',','],['),']') LIKE '%[{$_SESSION['login_id']}]%' ";
-					}
-					
-					$stat = array("Pending","Started","On-Progress","On-Hold","Over Due","Done");
-					$qry = $conn->query("SELECT t.*,p.name as pname,p.start_date,p.status as pstatus, p.end_date,p.id as pid FROM task_list t inner join project_list p on p.id = t.project_id $where order by p.name asc");
+					$qry =$conn->query("SELECT * FROM task_list");
 					while($row= $qry->fetch_assoc()):
-						$trans = get_html_translation_table(HTML_ENTITIES,ENT_QUOTES);
-						unset($trans["\""], $trans["<"], $trans[">"], $trans["<h2"]);
-						$desc = strtr(html_entity_decode($row['description']),$trans);
-						$desc=str_replace(array("<li>","</li>"), array("",", "), $desc);
-						$tprog = $conn->query("SELECT * FROM task_list where project_id = {$row['pid']}")->num_rows;
-		                $cprog = $conn->query("SELECT * FROM task_list where project_id = {$row['pid']} and status = 3")->num_rows;
-						$prog = $tprog > 0 ? ($cprog/$tprog) * 100 : 0;
-		                $prog = $prog > 0 ?  number_format($prog,2) : $prog;
-		                $prod = $conn->query("SELECT * FROM user_productivity where project_id = {$row['pid']}")->num_rows;
-		                if($row['pstatus'] == 0 && strtotime(date('Y-m-d')) >= strtotime($row['start_date'])):
-		                if($prod  > 0  || $cprog > 0)
-		                  $row['pstatus'] = 2;
-		                else
-		                  $row['pstatus'] = 1;
-		                elseif($row['pstatus'] == 0 && strtotime(date('Y-m-d')) > strtotime($row['end_date'])):
-		                $row['pstatus'] = 4;
-		                endif;
+						?>
+					<tr>
+					<th class="text-center"><?php echo $i++ ?></th>
+						<td><p><b><?php echo ucwords($row['task_name']) ?></b></p></td>
+						<td><b><?php echo date("M d, Y",strtotime($row['start_date'])) ?></b></td>
+						<td><b><?php echo date("M d, Y",strtotime($row['end_date'])) ?></b></td>
+						<td class="text-center">
+							<button type="button" class="btn btn-default btn-sm btn-flat border-info wave-effect text-info dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+		                      Action
+		                    </button>
+		                    <div class="dropdown-menu" style="">
+		                      <a class="dropdown-item view_project" href="./index.php?page=view_project&id=<?php echo $row['id'] ?>" data-id="<?php echo $row['id'] ?>">View</a>
+		                      <div class="dropdown-divider"></div>
+		                      <?php if($_SESSION['login_type'] != 3): ?>
+		                      <a class="dropdown-item" href="./index.php?page=edit_project&id=<?php echo $row['id'] ?>">Edit</a>
+		                      <div class="dropdown-divider"></div>
+		                      <a class="dropdown-item delete_project" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>">Delete</a>
+		                  <?php endif; ?>
+		                    </div>
+							  </td>
+							  </tr>
+							  <?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+<style>
+					/* // $where = "";
+					// if($_SESSION['login_type'] == 2){
+					// 	$where = " where p.manager_id = '{$_SESSION['login_id']}' ";
+					// }elseif($_SESSION['login_type'] == 3){
+					// 	$where = " where concat('[',REPLACE(p.user_ids,',','],['),']') LIKE '%[{$_SESSION['login_id']}]%' ";
+					// }
+					
+					// $stat = array("Pending","Started","On-Progress","On-Hold","Over Due","Done");
+					// $qry = $conn->query("SELECT t.*,p.name as pname,p.start_date,p.status as pstatus, p.end_date,p.id as pid FROM task_list t inner join project_list p on p.id = t.project_id $where order by p.name asc");
+					// while($row= $qry->fetch_assoc()):
+					// 	$trans = get_html_translation_table(HTML_ENTITIES,ENT_QUOTES);
+					// 	unset($trans["\""], $trans["<"], $trans[">"], $trans["<h2"]);
+					// 	$desc = strtr(html_entity_decode($row['description']),$trans);
+					// 	$desc=str_replace(array("<li>","</li>"), array("",", "), $desc);
+					// 	$tprog = $conn->query("SELECT * FROM task_list where project_id = {$row['pid']}")->num_rows;
+		            //     $cprog = $conn->query("SELECT * FROM task_list where project_id = {$row['pid']} and status = 3")->num_rows;
+					// 	$prog = $tprog > 0 ? ($cprog/$tprog) * 100 : 0;
+		            //     $prog = $prog > 0 ?  number_format($prog,2) : $prog;
+		            //     $prod = $conn->query("SELECT * FROM user_productivity where project_id = {$row['pid']}")->num_rows;
+		            //     if($row['pstatus'] == 0 && strtotime(date('Y-m-d')) >= strtotime($row['start_date'])):
+		            //     if($prod  > 0  || $cprog > 0)
+		            //       $row['pstatus'] = 2;
+		            //     else
+		            //       $row['pstatus'] = 1;
+		            //     elseif($row['pstatus'] == 0 && strtotime(date('Y-m-d')) > strtotime($row['end_date'])):
+		            //     $row['pstatus'] = 4;
+		            //     endif; */
 
-
+/* 
 					?>
 					<tr>
 						<td class="text-center"><?php echo $i++ ?></td>
@@ -111,13 +130,13 @@
 								</div>
 						</td>
 					</tr>	
-				<?php endwhile; ?>
+			
 				</tbody>
 			</table>
 		</div>
 	</div>
 </div>
-<style>
+<style> */
 	table p{
 		margin: unset !important;
 	}
@@ -127,7 +146,7 @@
 </style>
 <script>
 	$(document).ready(function(){
-		$('#list').dataTable()
+		// $('#list').dataTable()
 	$('.new_productivity').click(function(){
 		uni_modal("<i class='fa fa-plus'></i> New Progress for: "+$(this).attr('data-task'),"manage_progress.php?pid="+$(this).attr('data-pid')+"&tid="+$(this).attr('data-tid'),'large')
 	})
